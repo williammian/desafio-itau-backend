711031327796a399f918d38c627d65216e030da9
William Mian <william_mian@yahoo.com.br>
2025-04-25 13:54:53 -0300

    logs

--- desafio-itau/src/main/java/br/com/wm/desafioitau/service/EstatisticaService.java
+++ desafio-itau/src/main/java/br/com/wm/desafioitau/service/EstatisticaService.java
@@ -5,19 +5,26 @@ import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.wm.desafioitau.dto.EstatisticaDTO;
+import br.com.wm.desafioitau.exception.GlobalExceptionHandler;
 import br.com.wm.desafioitau.model.Transacao;
 
 @Service
 public class EstatisticaService {
 	
+	private static final Logger logger = LoggerFactory.getLogger(EstatisticaService.class);
+	
 	@Autowired
 	TransacaoService transacaoService;
 	
-	public EstatisticaDTO obterEstatistica() {		
+	public EstatisticaDTO obterEstatistica() {
+		long inicio = System.nanoTime(); // Início da medição
+		
 		OffsetDateTime agora = OffsetDateTime.now();
         OffsetDateTime limite = agora.minusSeconds(60);
         
@@ -33,6 +40,10 @@ public class EstatisticaService {
         	DoubleSummaryStatistics stats = transacoes.stream().mapToDouble(t -> t.getValor().doubleValue()).summaryStatistics();
         	estatisticaDTO = EstatisticaDTO.fromDoubleSumaryStatistics(stats);
         }
+        
+        long fim = System.nanoTime(); // Fim da medição
+        long duracaoEmMicros = (fim - inicio) / 1_000; // microssegundos
+        logger.info("Tempo de execução (Estatística): " + duracaoEmMicros + " µs");
         
         return estatisticaDTO;
 	}
