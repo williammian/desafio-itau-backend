90d6f2442fcba4055c6d2d8511283f91e3b0f1e2
William Mian <william_mian@yahoo.com.br>
2025-04-25 13:43:11 -0300

    logs

--- desafio-itau/src/main/java/br/com/wm/desafioitau/exception/GlobalExceptionHandler.java
+++ desafio-itau/src/main/java/br/com/wm/desafioitau/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package br.com.wm.desafioitau.exception;
 
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -9,11 +11,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 	@ExceptionHandler(ValidacaoException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(ValidacaoException ex) {
@@ -31,18 +34,21 @@ public class GlobalExceptionHandler {
     
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(HttpMessageNotReadableException ex) {
+    	logger.error("Bad request: {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = ErrorResponse.badRequestError(ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
     
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NoResourceFoundException ex) {
+    	logger.error("Resource not found: {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = ErrorResponse.notFoundError(ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 	
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleInternalServerError(Exception ex) {
+    	logger.error("Internal server error: {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = ErrorResponse.internalServerError(ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
